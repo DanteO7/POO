cefa18a8dfd1f40aff576e8c113eb985449e4c73
Dante07 <dante.orsetti@gmail.com>
2024-08-06 22:37:04 -0300

    Clases Programacion 2

---
+++ Empleado2/Program.cs
@@ -0,0 +1,38 @@
+﻿class Empleado
+{
+    private string _nombre;
+    private decimal _salario;
+    public string Nombre { get { return _nombre; } set { _nombre = value; } }
+    protected decimal Salario { get { return _salario; } set { if (value >= 0) { _salario = value; } else { Console.WriteLine("El salario no puede ser negativo"); } } }
+    public Empleado(string nombre, decimal salario)
+    {
+        Nombre = nombre;
+        Salario = salario;
+    }
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"\nNombre: {Nombre}, Salario: {Salario}");
+    }
+}
+class Gerente : Empleado
+{
+    public string Departamento { get; set; }
+    public Gerente(string nombre, decimal salario, string departamento) : base(nombre, salario)
+    {
+        Departamento = departamento;
+    }
+    public void MostrarInformacionGerente()
+    {
+        MostrarInformacion();
+        Console.WriteLine($"Departamento: {Departamento}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Gerente gerente = new Gerente("Dante", 6000, "TI");
+        gerente.MostrarInformacion();
+        gerente.MostrarInformacionGerente();
+    }
+}
